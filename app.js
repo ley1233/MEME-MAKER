7165c66a0f560c8b9600de7733b80b34e9e9b376
ley1233 <113224183+ley1233@users.noreply.github.com>
2022-12-27 07:00:41 +0900

    last

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const colorOptions = Array.from(document.getElementsByClassName("color-option"))
 const modeBtn = document.getElementById("mode-btn") 
 const destroyBtn = document.getElementById("destroy-btn")
 const eraseBtn = document.getElementById("eraser-btn")
+const fileInput = document.getElementById("file")
+const textInput = document.getElementById("text") 
+const saveBtn = document.getElementById("save")
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
@@ -14,6 +17,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width=800;
 canvas.height=800; 
 ctx.lineWidth = Linewidth.value;
+ctx.lineCap ="round";
 let isPainting = false;
 let isFilling =  false;
 
@@ -87,6 +91,38 @@ function onEraseClick(){
   
 }
 
+function onFileChange(event){
+const file = event.target.files[0];
+const url = URL.createObjectURL(file)
+const image = new Image()
+image.src = url
+image.onload = function(){
+ctx.drawImage(image, 0, 0, 800,800)
+fileInput.value =null;  
+}
+}
+
+function onDoubleClick(event){
+  
+  const text = textInput.value;
+  if (text !==""){
+  ctx.save();
+  ctx.lineWidth =1;
+  ctx.font = "48px serif";
+  ctx.fillText(text,event.offsetX,event.offsetY );
+  ctx.restore();
+  }
+}
+
+function onSaveClick(){
+  const url =  canvas.toDataURL();
+  const a = document.createElement("a")
+  a.href = url 
+  a.download =  "myDrawing.png"
+  a.click(); 
+}
+
+canvas.addEventListener("dblclick", onDoubleClick)
 canvas.addEventListener("mousemove",onMove)
 canvas.addEventListener("mousedown",onDown)
 canvas.addEventListener("mouseup", onUp)
@@ -102,3 +138,5 @@ canvas.addEventListener("click", onCanvasClick)
 
 destroyBtn.addEventListener("click",onDestroyClick)
 eraseBtn.addEventListener("click",onEraseClick)
+fileInput.addEventListener("change",onFileChange)
+saveBtn.addEventListener("click",onSaveClick)
